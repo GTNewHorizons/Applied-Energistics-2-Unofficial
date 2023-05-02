2875408b6fe8f1072b40ca74125afcac47b0a80e
Firenoo <49818773+firenoo@users.noreply.github.com>
2023-05-02 10:19:06 -0700

    Add the advanced blocking mode as a setting.
    Advanced Blocking Mode is now a setting to toggle between two modes:
    
    - Default: Exhibits the same behavior as normal blocking mode, ignores
    circuits.
    
    - Any: Blocks on any item, including circuits. I'm leaving this here in
    case of some niche behavior someone might want, but the use case is very
    uncommon (maybe some weird jig with taking circuits on demand...).

--- src/main/java/appeng/util/inv/AdaptorDualityInterface.java
+++ src/main/java/appeng/util/inv/AdaptorDualityInterface.java
@@ -2,7 +2,11 @@ package appeng.util.inv;
 
 import net.minecraft.inventory.IInventory;
 
+import appeng.api.config.AdvancedBlockingMode;
+import appeng.api.config.Settings;
 import appeng.api.config.Upgrades;
+import appeng.api.storage.data.IAEItemStack;
+import appeng.api.storage.data.IItemList;
 import appeng.helpers.DualityInterface;
 import appeng.tile.misc.TileInterface;
 
@@ -20,7 +24,19 @@ public class AdaptorDualityInterface extends AdaptorIInventory {
         DualityInterface dual = tileInterface.getInterfaceDuality();
         boolean hasMEItems = false;
         if (dual.getInstalledUpgrades(Upgrades.ADVANCED_BLOCKING) > 0) {
-            hasMEItems = !dual.getItemInventory().getStorageList().isEmpty();
+            if (dual.getConfigManager().getSetting(Settings.ADVANCED_BLOCKING_MODE) == AdvancedBlockingMode.DEFAULT) {
+                IItemList<IAEItemStack> itemList = dual.getItemInventory().getStorageList();
+                // This works okay, it'll loop as much as (or even less than) a normal inventory because the iterator
+                // hides empty slots or stacks of size 0
+                for (IAEItemStack stack : itemList) {
+                    if (!stack.getItem().getUnlocalizedName().equals("gt.integrated_circuit")) {
+                        hasMEItems = true;
+                        break;
+                    }
+                }
+            } else {
+                hasMEItems = !dual.getItemInventory().getStorageList().isEmpty();
+            }
         }
         return hasMEItems || super.containsItems();
     }
