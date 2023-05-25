777a575882d88fc54d552677e4a1da73c869851a
GitHub GTNH Actions <>
2023-05-25 18:19:21 +0000

    spotlessApply

--- src/main/java/appeng/integration/modules/NEIHelpers/NEIGrinderRecipeHandler.java
+++ src/main/java/appeng/integration/modules/NEIHelpers/NEIGrinderRecipeHandler.java
@@ -17,7 +17,6 @@ import java.util.List;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.inventory.GuiContainer;
-import net.minecraft.inventory.Container;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 
@@ -31,8 +30,6 @@ import appeng.core.localization.GuiText;
 import codechicken.lib.gui.GuiDraw;
 import codechicken.nei.NEIServerUtils;
 import codechicken.nei.PositionedStack;
-import codechicken.nei.api.IOverlayHandler;
-import codechicken.nei.api.IRecipeOverlayRenderer;
 import codechicken.nei.recipe.TemplateRecipeHandler;
 
 public class NEIGrinderRecipeHandler extends TemplateRecipeHandler {
