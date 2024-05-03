c3f4e76f35f5183d8b1bcbd72d254fe2a46e7b3d
Caedis <Caedis@users.noreply.github.com>
2024-05-03 14:12:49 -0500

    Rework icon null checks

---
+++ src/main/java/appeng/client/texture/TextureUtils.java
@@ -0,0 +1,55 @@
+package appeng.client.texture;
+
+import javax.annotation.Nonnull;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.util.IIcon;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class TextureUtils {
+
+    @SideOnly(Side.CLIENT)
+    @Nonnull
+    public static IIcon checkTexture(IIcon val) {
+        return checkTexture(val, true);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Nonnull
+    public static IIcon checkTexture(IIcon val, boolean isBlock) {
+        if (val == null) {
+            val = isBlock ? getMissingBlock() : getMissingItem();
+        }
+
+        assert val != null;
+
+        return val;
+    }
+
+    private static TextureAtlasSprite missingBlockTexture = null;
+    private static TextureAtlasSprite missingItemTexture = null;
+
+    @SideOnly(Side.CLIENT)
+    public static IIcon getMissingBlock() {
+        if (missingBlockTexture == null) {
+            missingBlockTexture = ((TextureMap) Minecraft.getMinecraft().getTextureManager()
+                    .getTexture(TextureMap.locationBlocksTexture)).getAtlasSprite("missingno");
+        }
+
+        return missingBlockTexture;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public static IIcon getMissingItem() {
+        if (missingItemTexture == null) {
+            missingItemTexture = ((TextureMap) Minecraft.getMinecraft().getTextureManager()
+                    .getTexture(TextureMap.locationItemsTexture)).getAtlasSprite("missingno");
+        }
+
+        return missingItemTexture;
+    }
+}
