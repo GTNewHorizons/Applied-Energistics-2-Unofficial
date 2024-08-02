03f219d4982b537b7cc0de5b22ff8e398ba8e0c0
Demiu <miazgapow@gmail.com>
2024-08-03 00:00:00 +0200

    Restore api backwards compatibility

--- src/main/java/appeng/util/IterationCounter.java
+++ src/main/java/appeng/util/IterationCounter.java
@@ -5,8 +5,38 @@ import java.util.concurrent.atomic.AtomicInteger;
 public final class IterationCounter {
 
     private static AtomicInteger counter = new AtomicInteger(0);
+    private static int globalDepth = 0;
+    private static int globalCounter = 0;
 
     public static int fetchNewId() {
         return counter.getAndIncrement();
+    }
+
+    /**
+     * Increment the global iteration depth
+     * 
+     * @return Iteration number currently used for global iteration
+     */
+    public static synchronized int incrementGlobalDepth() {
+        if (globalDepth == 0) {
+            globalCounter++;
+        }
+        globalDepth++;
+        return globalCounter;
+    }
+
+    /**
+     * Initialize global iteration number and increment global depth This unconditionally overrides current global
+     * iteration number!
+     *
+     * @param iteration Iteration number to set global one to
+     */
+    public static synchronized void incrementGlobalDepthWith(int iteration) {
+        globalCounter = iteration;
+        globalDepth++;
+    }
+
+    public static synchronized void decrementGlobalDepth() {
+        globalDepth--;
     }
 }
