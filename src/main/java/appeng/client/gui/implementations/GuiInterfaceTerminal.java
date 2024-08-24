ef69454473433588b3a83d5006577b01b3d1ee95
hiroscho <hiroscho.games@gmail.com>
2024-08-24 09:52:39 +0200

    apply spotless

--- src/main/java/appeng/client/gui/implementations/GuiInterfaceTerminal.java
+++ src/main/java/appeng/client/gui/implementations/GuiInterfaceTerminal.java
@@ -1143,7 +1143,7 @@ public class GuiInterfaceTerminal extends AEBaseGui
             String output = GuiInterfaceTerminal.this.searchFieldOutputs.getText().toLowerCase();
 
             for (InterfaceTerminalEntry entry : entries) {
-                if(!entry.online) continue;
+                if (!entry.online) continue;
 
                 var moleAss = AEApi.instance().definitions().blocks().molecularAssembler().maybeStack(1);
                 entry.dispY = -9999;
