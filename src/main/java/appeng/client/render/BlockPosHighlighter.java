facd387a64120f08a374f205c5682dc19266c846
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2024-11-13 02:14:43 +0100

    merge BlockPosHighlighter and HighlighterHandler into a single class

--- src/main/java/appeng/client/render/BlockPosHighlighter.java
+++ src/main/java/appeng/client/render/BlockPosHighlighter.java
@@ -3,13 +3,20 @@ package appeng.client.render;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.MathHelper;
+import net.minecraftforge.client.event.RenderWorldLastEvent;
+
+import org.lwjgl.opengl.GL11;
 
 import appeng.api.util.DimensionalCoord;
 import appeng.api.util.WorldCoord;
 import appeng.core.localization.PlayerMessages;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 // taken from McJty's McJtyLib
 public class BlockPosHighlighter {
@@ -44,16 +51,87 @@ public class BlockPosHighlighter {
         expireHighlightTime = System.currentTimeMillis() + highlightDuration;
     }
 
-    public static List<DimensionalCoord> getHighlightedBlocks() {
-        return highlightedBlocks;
-    }
-
-    public static void clear() {
+    private static void clear() {
         highlightedBlocks.clear();
         expireHighlightTime = -1;
     }
 
-    public static long getExpireHighlightTime() {
-        return expireHighlightTime;
+    @SubscribeEvent
+    public void renderHighlightedBlocks(RenderWorldLastEvent event) {
+        if (highlightedBlocks.isEmpty()) {
+            return;
+        }
+        long time = System.currentTimeMillis();
+        if (time > expireHighlightTime) {
+            clear();
+            return;
+        }
+        if (((time / 500) & 1) == 0) {
+            // this does the blinking effect
+            return;
+        }
+        Minecraft mc = Minecraft.getMinecraft();
+        int dimension = mc.theWorld.provider.dimensionId;
+
+        EntityPlayerSP p = mc.thePlayer;
+        double doubleX = p.lastTickPosX + (p.posX - p.lastTickPosX) * event.partialTicks;
+        double doubleY = p.lastTickPosY + (p.posY - p.lastTickPosY) * event.partialTicks;
+        double doubleZ = p.lastTickPosZ + (p.posZ - p.lastTickPosZ) * event.partialTicks;
+
+        for (DimensionalCoord c : highlightedBlocks) {
+            if (dimension != c.getDimension()) {
+                continue;
+            }
+            GL11.glPushMatrix();
+            GL11.glPushAttrib(GL11.GL_ALL_ATTRIB_BITS);
+            GL11.glLineWidth(3);
+            GL11.glTranslated(-doubleX, -doubleY, -doubleZ);
+
+            GL11.glDisable(GL11.GL_DEPTH_TEST);
+            GL11.glDisable(GL11.GL_TEXTURE_2D);
+
+            renderHighLightedBlocksOutline(c.x, c.y, c.z);
+
+            GL11.glPopAttrib();
+            GL11.glPopMatrix();
+        }
+    }
+
+    private static void renderHighLightedBlocksOutline(double x, double y, double z) {
+        Tessellator tess = Tessellator.instance;
+        tess.startDrawing(GL11.GL_LINE_STRIP);
+
+        tess.setColorRGBA_F(1.0f, 0.0f, 0.0f, 1.0f);
+
+        tess.addVertex(x, y, z);
+        tess.addVertex(x, y + 1, z);
+        tess.addVertex(x, y + 1, z + 1);
+        tess.addVertex(x, y, z + 1);
+        tess.addVertex(x, y, z);
+
+        tess.addVertex(x + 1, y, z);
+        tess.addVertex(x + 1, y + 1, z);
+        tess.addVertex(x + 1, y + 1, z + 1);
+        tess.addVertex(x + 1, y, z + 1);
+        tess.addVertex(x + 1, y, z);
+
+        tess.addVertex(x, y, z);
+        tess.addVertex(x + 1, y, z);
+        tess.addVertex(x + 1, y, z + 1);
+        tess.addVertex(x, y, z + 1);
+        tess.addVertex(x, y + 1, z + 1);
+        tess.addVertex(x + 1, y + 1, z + 1);
+        tess.addVertex(x + 1, y + 1, z);
+        tess.addVertex(x + 1, y, z);
+        tess.addVertex(x, y, z);
+        tess.addVertex(x + 1, y, z);
+        tess.addVertex(x + 1, y + 1, z);
+        tess.addVertex(x, y + 1, z);
+        tess.addVertex(x, y + 1, z + 1);
+        tess.addVertex(x + 1, y + 1, z + 1);
+        tess.addVertex(x + 1, y, z + 1);
+        tess.addVertex(x, y, z + 1);
+
+        tess.draw();
     }
 }
