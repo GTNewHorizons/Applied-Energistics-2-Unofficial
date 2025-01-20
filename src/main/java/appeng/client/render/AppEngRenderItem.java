cec5563210b4432472cfc5f13bb82356780df80e
yoshy2002 <jascha.morzuch@gmail.com>
2025-01-20 15:05:14 +0100

    change check

--- src/main/java/appeng/client/render/AppEngRenderItem.java
+++ src/main/java/appeng/client/render/AppEngRenderItem.java
@@ -102,7 +102,7 @@ public class AppEngRenderItem extends AERenderItem {
 
             // Display stack quantity
             final long amount = this.aeStack != null ? this.aeStack.getStackSize() : is.stackSize;
-            if (amount > 0 && showStackSize || (!this.aeStack.isCraftable() && amount == 0)) {
+            if (showStackSize && (amount > 0 || !this.aeStack.isCraftable())) {
                 GL11.glDisable(GL11.GL_LIGHTING);
                 GL11.glPushMatrix();
                 this.drawStackSize(par4, par5, amount, fontRenderer, fontSize);
