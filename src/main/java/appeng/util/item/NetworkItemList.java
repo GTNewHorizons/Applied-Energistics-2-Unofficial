13b67e0178be4492980a8268c5be6e164ea2a9e3
hiroscho <hiroscho.games@gmail.com>
2025-10-04 22:16:17 +0200

    Fix wildcard import

--- src/main/java/appeng/util/item/NetworkItemList.java
+++ src/main/java/appeng/util/item/NetworkItemList.java
@@ -10,7 +10,12 @@
 
 package appeng.util.item;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
