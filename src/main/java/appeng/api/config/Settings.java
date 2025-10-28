6b626299b1de8b8caaa63a0045ee641d50ff1b6e
SST-03 <70825063+SST-03@users.noreply.github.com>
2025-10-28 16:31:16 +0800

    Spotless

--- src/main/java/appeng/api/config/Settings.java
+++ src/main/java/appeng/api/config/Settings.java
@@ -89,7 +89,7 @@ public enum Settings {
     CRAFTING_ALLOW(EnumSet.allOf(CraftingAllow.class)),
 
     PINS_STATE(EnumSet.allOf(PinsState.class)),
-    
+
     PAUSE_WHEN_HOLDING_SHIFT(EnumSet.of(YesNo.YES, YesNo.NO));
 
     private final EnumSet<? extends Enum<?>> values;
