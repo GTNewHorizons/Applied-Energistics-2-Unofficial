2c40503773b48c7a49328dac573ecab989b7fac9
Martin Robertz <dream-master@gmx.net>
2025-11-07 20:00:59 +0100

    Universal gui (#910)
    Co-authored-by: lc_1337 <lordicocain@gmail.com>
    Co-authored-by: Kogepan229 <kogepan25629@gmail.com>
    Co-authored-by: lc-1337 <62835225+lc-1337@users.noreply.github.com>
    Co-authored-by: Kogepan229 <73163994+Kogepan229@users.noreply.github.com>

--- src/main/java/appeng/util/Platform.java
+++ src/main/java/appeng/util/Platform.java
@@ -112,7 +112,7 @@ import appeng.api.storage.data.IAETagCompound;
 import appeng.api.storage.data.IItemList;
 import appeng.api.util.AEColor;
 import appeng.api.util.DimensionalCoord;
-import appeng.client.me.SlotME;
+import appeng.container.AEBaseContainer;
 import appeng.container.slot.SlotFake;
 import appeng.core.AEConfig;
 import appeng.core.AELog;
@@ -176,6 +176,7 @@ public class Platform {
     private static final DecimalFormat df = new DecimalFormat("#.##");
     public static final boolean isAE2FCLoaded = Loader.isModLoaded("ae2fc");
     public static final boolean isEIOLoaded = Loader.isModLoaded("EnderIO");
+    public static boolean isBaublesLoaded = Loader.isModLoaded("Baubles");
 
     static {
         BYTE_LIMIT = new double[10];
@@ -341,6 +342,11 @@ public class Platform {
 
     public static void openGUI(@Nonnull final EntityPlayer p, @Nullable final TileEntity tile,
             @Nullable final ForgeDirection side, @Nonnull final GuiBridge type) {
+        openGUI(p, tile, side, type, -1);
+    }
+
+    public static void openGUI(@Nonnull final EntityPlayer p, @Nullable final TileEntity tile,
+            @Nullable final ForgeDirection side, @Nonnull final GuiBridge type, final int slotIndex) {
         if (isClient()) {
             return;
         }
@@ -355,13 +361,13 @@ public class Platform {
         }
 
         if ((type.getType().isItem() && tile == null) || type.hasPermissions(tile, x, y, z, side, p)) {
-            if (tile == null && type.getType() == GuiHostType.ITEM) {
+            if (tile == null && type.getType() != GuiHostType.WORLD) {
                 p.openGui(
                         AppEng.instance(),
                         type.ordinal() << 5 | (1 << 4),
                         p.getEntityWorld(),
-                        p.inventory.currentItem,
-                        0,
+                        slotIndex == -1 ? p.inventory.currentItem : slotIndex,
+                        p.openContainer instanceof AEBaseContainer abc ? abc.getSwitchAbleGuiNext() : -1,
                         0);
             } else if (tile == null || type.getType() == GuiHostType.ITEM) {
                 p.openGui(AppEng.instance(), type.ordinal() << 5 | (1 << 3), p.getEntityWorld(), x, y, z);
@@ -792,7 +798,7 @@ public class Platform {
             return "** Null";
         }
 
-        final String n = ((AEItemStack) is).getModID();
+        final String n = ((AEItemStack) is).getModId();
         return n == null ? "** Null" : n;
     }
 
@@ -1257,13 +1263,20 @@ public class Platform {
         if (possible != null) {
             retrieved = possible.getStackSize();
         }
+        final int typeMultiplier = request.getPowerMultiplier();
 
-        final double availablePower = energy.extractAEPower(retrieved, Actionable.SIMULATE, PowerMultiplier.CONFIG);
+        final double availablePower = energy.extractAEPower(
+                Platform.ceilDiv(retrieved, typeMultiplier),
+                Actionable.SIMULATE,
+                PowerMultiplier.CONFIG);
 
-        final long itemToExtract = Math.min((long) (availablePower + 0.9), retrieved);
+        final long itemToExtract = Math.min((long) (availablePower * typeMultiplier + 0.9), retrieved);
 
         if (itemToExtract > 0) {
-            energy.extractAEPower(retrieved, Actionable.MODULATE, PowerMultiplier.CONFIG);
+            energy.extractAEPower(
+                    Platform.ceilDiv(retrieved, typeMultiplier),
+                    Actionable.MODULATE,
+                    PowerMultiplier.CONFIG);
 
             possible.setStackSize(itemToExtract);
             final StackType ret = cell.extractItems(possible, Actionable.MODULATE, src);
@@ -1286,7 +1299,7 @@ public class Platform {
         if (possible != null) {
             stored -= possible.getStackSize();
         }
-        long typeMultiplier = input instanceof IAEFluidStack ? 1000 : 1;
+        final int typeMultiplier = input.getPowerMultiplier();
 
         final double availablePower = energy
                 .extractAEPower(Platform.ceilDiv(stored, typeMultiplier), Actionable.SIMULATE, PowerMultiplier.CONFIG);
@@ -1373,8 +1386,8 @@ public class Platform {
     }
 
     public static <T extends IAEStack<T>> void postListChanges(final IItemList<T> before, final IItemList<T> after,
-            final IMEMonitorHandlerReceiver<T> meMonitorPassthrough, final BaseActionSource source) {
-        final LinkedList<T> changes = new LinkedList<>();
+            final IMEMonitorHandlerReceiver meMonitorPassthrough, final BaseActionSource source) {
+        final LinkedList<IAEStack<?>> changes = new LinkedList<>();
 
         for (final T is : before) {
             is.setStackSize(-is.getStackSize());
@@ -1846,10 +1859,6 @@ public class Platform {
             return null;
         }
 
-        if (slot instanceof SlotME) {
-            return ((SlotME) slot).getAEStack();
-        }
-
         if (slot instanceof SlotFake) {
             return ((SlotFake) slot).getAEStack();
         }
@@ -2000,6 +2009,7 @@ public class Platform {
     }
 
     public static IAEStack<?> readStackNBT(NBTTagCompound tag, boolean convert) {
+        if (tag == null) return null;
         if (tag.hasKey("StackType", 1)) {
             // For old experimental compatibility
             final byte stackType = tag.getByte("StackType");
@@ -2088,6 +2098,13 @@ public class Platform {
             return AEFluidStack.create(ItemFluidPacket.getFluidStack(stack));
         }
         return AEItemStack.create(stack);
+    }
+
+    public static boolean isStacksIdentical(IAEStack<?> a, IAEStack<?> b) {
+        if (a == null && b == null) return true;
+        if (a == null || b == null) return false;
+        if (a.equals(b)) return a.getStackSize() == b.getStackSize();
+        return false;
     }
 
     public static int longToInt(long number) {
