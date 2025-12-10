4e25840ff9a073ffe135f1997767e3666e17500b
zyf051520 <zyf051520@foxmail.com>
2025-12-10 19:13:30 +0800

    check subsitude valid

--- src/main/java/appeng/crafting/v2/resolvers/CraftableItemResolver.java
+++ src/main/java/appeng/crafting/v2/resolvers/CraftableItemResolver.java
@@ -141,6 +141,10 @@ public class CraftableItemResolver<StackType extends IAEStack<StackType>>
 
             if (matchingOutput == null) {
                 state = State.FAILURE;
+            } else if (!request.acceptableSubstituteFn.test(this.matchingOutput)) {
+                // A pattern that accepts substitutes might not accepts all fuzzy items found.
+                // Set to State.FAILURE if test fails, to prevent it from being added to tasks.
+                state = State.FAILURE;
             }
         }
 
@@ -308,6 +312,7 @@ public class CraftableItemResolver<StackType extends IAEStack<StackType>>
                 state = State.SUCCESS;
                 return new StepOutput(Collections.emptyList());
             }
+
             final boolean canUseSubstitutes = pattern.canSubstitute();
             final SubstitutionMode childMode = canUseSubstitutes ? SubstitutionMode.ACCEPT_FUZZY
                     : SubstitutionMode.PRECISE;
