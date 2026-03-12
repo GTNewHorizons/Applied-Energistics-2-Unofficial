de988cb0ddcdd1e4cc31ce3c6d85aa4f33b379d4
Bryan Koch <bbkoch94@gmail.com>
2026-03-12 00:25:00 -0700

    optimize default IMEInventory.getAvailableItem() to use the new filter parameter of getAvailableItems

--- src/main/java/appeng/api/storage/IMEInventory.java
+++ src/main/java/appeng/api/storage/IMEInventory.java
@@ -147,7 +147,10 @@ public interface IMEInventory<StackType extends IAEStack> {
     @SuppressWarnings("unchecked") // changing the generic StackType to be correct here is too much of a breaking API
                                    // change
     default StackType getAvailableItem(@Nonnull StackType request, int iteration) {
-        return getAvailableItems((IItemList<StackType>) getStackType().createList(), iteration).findPrecise(request);
+        return getAvailableItems(
+                (IItemList<StackType>) getStackType().createList(),
+                iteration,
+                Optional.of(s -> s.isSameType(request))).findPrecise(request);
     }
 
     /**
