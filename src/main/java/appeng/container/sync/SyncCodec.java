a51316fc72d93d9bc3f0a7375a9b2698553ad7ad
Kogepan229 <73163994+Kogepan229@users.noreply.github.com>
2026-05-13 17:59:36 +0900

    Add Action handler (#1276)

--- src/main/java/appeng/container/sync/SyncCodec.java
+++ src/main/java/appeng/container/sync/SyncCodec.java
@@ -1,32 +1,12 @@
 package appeng.container.sync;
 
-import java.io.IOException;
-
-import io.netty.buffer.ByteBuf;
-
 /**
  * Encodes and decodes values synchronized by {@link SyncRegistrar} object handlers.
  * <p>
  * Implementations define both the packet format and the value semantics used by the synchronization system to detect
  * changes. Use {@link SyncCodecs} for common codecs and factory methods.
  */
-public interface SyncCodec<T> {
-
-    @FunctionalInterface
-    interface Reader<T> {
-
-        T read(ByteBuf buf) throws IOException;
-    }
-
-    @FunctionalInterface
-    interface Writer<T> {
-
-        void write(ByteBuf buf, T value) throws IOException;
-    }
-
-    void write(ByteBuf buf, T value) throws IOException;
-
-    T read(ByteBuf buf) throws IOException;
+public interface SyncCodec<T> extends StreamCodec<T> {
 
     /**
      * Returns an isolated copy that can be retained as the last synchronized value.
@@ -38,10 +18,4 @@ public interface SyncCodec<T> {
      */
     boolean valuesEqual(T a, T b);
 
-    /**
-     * Returns the stable type identifier used when validating that both sides registered compatible handlers.
-     */
-    default String getTypeKey() {
-        return this.getClass().getName();
-    }
 }
