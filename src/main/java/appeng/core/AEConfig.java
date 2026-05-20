16fc59252a39c6dd817e4b58ba8c37bab75d7cd2
lc-1337 <62835225+lc-1337@users.noreply.github.com>
2026-05-20 11:05:45 +0300

    Add Super Wireless Kit (#734)
    Co-authored-by: lordIcocain <62835225+lordIcocain@users.noreply.github.com>
    Co-authored-by: Maya <10861407+serenibyss@users.noreply.github.com>
    Co-authored-by: Martin Robertz <dream-master@gmx.net>
    Co-authored-by: NeOzay <colpaert.benoit@gmail.com>
    Co-authored-by: Ethryan <3237986+Ethryan@users.noreply.github.com>
    Co-authored-by: NeOzay <69015205+NeOzay@users.noreply.github.com>
    Co-authored-by: PLASMAchicken <plasmachickenmc@gmail.com>
    Co-authored-by: Kogepan229 <kogepan25629@gmail.com>

--- src/main/java/appeng/core/AEConfig.java
+++ src/main/java/appeng/core/AEConfig.java
@@ -129,6 +129,10 @@ public final class AEConfig extends Configuration implements IConfigurableObject
     private double WirelessBaseRange = 16;
     private double WirelessBoosterRangeMultiplier = 1;
     private double WirelessBoosterExp = 1.5;
+    private double WirelessConnectorPowerBase = 1D;
+    private double WirelessConnectorPowerDistanceMultiplier = .1D;
+    public float visualiserWidthDense;
+    public float visualiserWidthNormal;
     public int levelEmitterDelay = 40;
     public int craftingCalculatorVersion = 2;
     public int maxCraftingSteps = 2_000_000;
@@ -228,6 +232,15 @@ public final class AEConfig extends Configuration implements IConfigurableObject
                 .getDouble(this.WirelessBoosterRangeMultiplier);
         this.WirelessBoosterExp = this.get("wireless", "WirelessBoosterExp", this.WirelessBoosterExp)
                 .getDouble(this.WirelessBoosterExp);
+        this.WirelessConnectorPowerBase = this
+                .get("wireless", "WirelessConnectorPowerBase", this.WirelessConnectorPowerBase)
+                .getDouble(this.WirelessConnectorPowerBase);
+        this.WirelessConnectorPowerDistanceMultiplier = this
+                .get(
+                        "wireless",
+                        "WirelessConnectorPowerDistanceMultiplier",
+                        this.WirelessConnectorPowerDistanceMultiplier)
+                .getDouble(this.WirelessConnectorPowerDistanceMultiplier);
         this.WirelessTerminalDrainMultiplier = this
                 .get("wireless", "WirelessTerminalDrainMultiplier", this.WirelessTerminalDrainMultiplier)
                 .getDouble(this.WirelessTerminalDrainMultiplier);
@@ -367,6 +380,8 @@ public final class AEConfig extends Configuration implements IConfigurableObject
                 .getBoolean(true);
         this.MEMonitorableSmallSize = this.get("Client", "MEMonitorableSmallSize", 6).getInt(6);
         this.InterfaceTerminalSmallSize = this.get("Client", "InterfaceTerminalSmallSize", 6).getInt(6);
+        this.visualiserWidthDense = (float) this.get("Client", "visualiserWidthDense", 16f).getDouble(16f);
+        this.visualiserWidthNormal = (float) this.get("Client", "visualiserWidthNormal", 4f).getDouble(4f);
 
         // Pin options (under Client category)
         Property pMaxCraft = this.get("Client", "maxCraftingPinRows", this.maxCraftingPinRows);
@@ -457,6 +472,14 @@ public final class AEConfig extends Configuration implements IConfigurableObject
     public double wireless_getPowerDrain(final int boosters) {
         return this.WirelessBaseCost
                 + this.WirelessCostMultiplier * Math.pow(boosters, 1 + boosters / this.WirelessHighWirelessCount);
+    }
+
+    public double getWirelessConnectorPowerBase() {
+        return this.WirelessConnectorPowerBase;
+    }
+
+    public double getWirelessConnectorPowerDistanceMultiplier() {
+        return this.WirelessConnectorPowerDistanceMultiplier;
     }
 
     @Override
