9a441c68bc4742444d17e96b5831adbe8b3bfe39
lc-1337 <lordicocain@gmail.com>
2026-05-23 22:17:18 +0300

    add keybinds for wireless keys

--- src/main/java/appeng/util/Platform.java
+++ src/main/java/appeng/util/Platform.java
@@ -69,11 +69,8 @@ import net.minecraftforge.common.util.FakePlayerFactory;
 import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.oredict.OreDictionary;
 
-import org.lwjgl.input.Keyboard;
-
 import com.glodblock.github.common.item.ItemFluidDrop;
 import com.glodblock.github.common.item.ItemFluidPacket;
-import com.gtnewhorizon.gtnhlib.keybind.SyncedKeybind;
 import com.mojang.authlib.GameProfile;
 
 import appeng.api.AEApi;
@@ -182,8 +179,6 @@ public class Platform {
     public static final boolean isBackhandLoaded = Loader.isModLoaded("backhand");
     public static final boolean isPosteaLoaded = Loader.isModLoaded("postea");
     public static final boolean isGTLoaded = IntegrationRegistry.INSTANCE.isEnabled(IntegrationType.GT);
-    public static final SyncedKeybind keyBindLCtrl = SyncedKeybind.create(Keyboard.KEY_LCONTROL);
-    public static final SyncedKeybind keyBindTab = SyncedKeybind.create(Keyboard.KEY_TAB);
     public static final boolean isThaumicEnergisticsLoaded = Loader.isModLoaded("thaumicenergistics");
     public static final boolean isEndlessIdsLoaded = Loader.isModLoaded("endlessids");
 
