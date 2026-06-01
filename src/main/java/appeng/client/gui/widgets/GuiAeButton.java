a1e384d237c94b1e1dd90bdde35ab456fbd009ae
Ranzu <66495944+Ranzuu@users.noreply.github.com>
2026-06-01 16:42:58 +0200

    Re-do AEButton - no more enums - correct auto sizing

--- src/main/java/appeng/client/gui/widgets/GuiAeButton.java
+++ src/main/java/appeng/client/gui/widgets/GuiAeButton.java
@@ -5,6 +5,8 @@ import java.util.regex.Pattern;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 
+import org.lwjgl.opengl.GL11;
+
 public class GuiAeButton extends GuiButton implements ITooltip {
 
     private static final Pattern PATTERN_NEW_LINE = Pattern.compile("\\n", Pattern.LITERAL);
@@ -33,6 +35,20 @@ public class GuiAeButton extends GuiButton implements ITooltip {
     @Override
     public void drawButton(Minecraft mc, int mouseX, int mouseY) {
         super.drawButton(mc, mouseX, mouseY);
+
+        if (!this.visible || this.height >= 20) return;
+        mc.getTextureManager().bindTexture(buttonTextures);
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+        int texV = 46 + this.getHoverState(this.field_146123_n) * 20;
+        int half = this.width / 2;
+        this.drawTexturedModalRect(this.xPosition, this.yPosition + this.height - 2, 0, texV + 18, half, 2);
+        this.drawTexturedModalRect(
+                this.xPosition + half,
+                this.yPosition + this.height - 2,
+                200 - half,
+                texV + 18,
+                half,
+                2);
     }
 
     @Override
