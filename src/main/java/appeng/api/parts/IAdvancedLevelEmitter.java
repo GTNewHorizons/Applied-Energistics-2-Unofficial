a563e9314ad98ea2e189909436d105d7646559f0
Ranzu <66495944+Ranzuu@users.noreply.github.com>
2026-07-19 23:23:04 +0200

    Prep

---
+++ src/main/java/appeng/api/parts/IAdvancedLevelEmitter.java
@@ -0,0 +1,27 @@
+package appeng.api.parts;
+
+import appeng.api.features.ILevelViewable;
+import appeng.api.implementations.IUpgradeableHost;
+import appeng.api.networking.storage.IStackWatcherHost;
+import appeng.api.networking.ticking.IGridTickable;
+import appeng.api.storage.IMEMonitorHandlerReceiver;
+import appeng.api.storage.data.IAEStack;
+import appeng.tile.inventory.IIAEStackInventory;
+
+public interface IAdvancedLevelEmitter extends IStackWatcherHost, IMEMonitorHandlerReceiver<IAEStack<?>>,
+        IGridTickable, IUpgradeableHost, IIAEStackInventory, ILevelViewable {
+
+    int SLOT_COUNT = 6;
+
+    long getReportingValue(int slot);
+
+    void setReportingValue(int slot, long v);
+
+    boolean isSlotActive(int slot);
+
+    void setSlotActive(int slot, boolean active);
+
+    boolean isSlotInverted(int slot);
+
+    void setSlotInverted(int slot, boolean inverted);
+}
