3510bdf342e737d36defea4be76ecc072de20ee1
GitHub GTNH Actions <>
2026-09-13 15:55:33 +0000

    spotlessApply

--- src/main/java/appeng/parts/PartPlacement.java
+++ src/main/java/appeng/parts/PartPlacement.java
@@ -278,7 +278,9 @@ public class PartPlacement {
                     IPartHost oppositeHost = getExistingHost(opposite);
                     if (oppositeHost != null
                             && oppositeHost.getPart(ForgeDirection.UNKNOWN) instanceof PartCable oppositeCable) {
-                        if (host.getPart(s) == null && !host.isBlocked(s) && oppositeHost.getPart(s.getOpposite()) == null && !oppositeHost.isBlocked(s.getOpposite())) {
+                        if (host.getPart(s) == null && !host.isBlocked(s)
+                                && oppositeHost.getPart(s.getOpposite()) == null
+                                && !oppositeHost.isBlocked(s.getOpposite())) {
                             if (host.getColor().matches(oppositeCable.getColor())) {
                                 cable.addConnection(s);
                                 oppositeCable.addConnection(s.getOpposite());
