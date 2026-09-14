57abf1b82acf9e0a41f3b93e6ee967c1c9644b19
Ranzu <66495944+Ranzuu@users.noreply.github.com>
2026-09-14 18:46:09 +0200

    Change the position of the inventory label inside the Interface terminal

--- src/main/java/appeng/client/gui/implementations/GuiInterfaceTerminal.java
+++ src/main/java/appeng/client/gui/implementations/GuiInterfaceTerminal.java
@@ -376,14 +376,14 @@ public class GuiInterfaceTerminal extends AEBaseGui
                 ColorUtils.guiTextColorGray.getColor());
         fontRendererObj.drawString(
                 GuiText.inventory.getLocal(),
-                GuiInterfaceTerminal.VIEW_LEFT + 2,
-                this.ySize - 96,
+                GuiInterfaceTerminal.VIEW_LEFT + 12,
+                this.ySize - 93,
                 ColorUtils.guiTextColorGray.getColor());
         if (!neiPresent && tooltipStack != null) {
             renderToolTip(tooltipStack, mouseX, mouseY);
         }
     }
-
+    
     @Override
     public void drawScreen(final int mouseX, final int mouseY, final float btn) {
         guiButtonAssemblersOnly.set(
